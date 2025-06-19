d708b2f0f697774cfb80382064b3fcc33806973b
ddInference <zzningxp@qq.com>
2025-06-19 10:35:24 +0800

    Initial commit

---
+++ run_tasks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# python -m hcsd.evaluation.gen_ea_answer_llama2chat --ea-model-path llama2_7b_draft --draft-model-type hcsd --base-model-path model/llama-2-7B-chat-hf --model-id humaneval_hcsd_7_4_10 --bench-name humaneval --total-token 7 --depth 4  --top-k 10 --num-gpus-per-model 1 --num-gpus-total 1  --temperature 0.0
